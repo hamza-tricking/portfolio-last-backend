d5b5a44d6103014df8a631b1b15e4044de93d493
Mohamed <hamzatricks@gmail.com>
2026-07-22 18:56:34 +0100

    first commit

--- models/User.js
+++ models/User.js
@@ -51,7 +51,8 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
-  this.password = await bcrypt.hash(this.password, 12);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
